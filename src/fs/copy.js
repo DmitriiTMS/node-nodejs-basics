7e40bcb64d923b8d1c3c1ae5676b183094169e8f
DmitriiTMS <dmitrii12_88@mail.ru>
2024-09-23 22:14:55 +0300

    feat: add copy and create file

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,30 @@
+import fs from "fs";
+
 const copy = async () => {
-    // Write your code here 
+  const filesfolderPath = import.meta.dirname + "\\files";
+
+  const filesCopyfolderPath = import.meta.dirname + "\\files_copy";
+
+  fs.access(filesfolderPath, (err) => {
+    if (err) {
+      throw new Error("FS operation failed");
+    } else {
+      fs.access(filesCopyfolderPath, (err) => {
+        if (err) {
+          fs.cp(
+            filesfolderPath,
+            filesCopyfolderPath,
+            { recursive: true },
+            (err) => {
+              return err;
+            }
+          );
+        } else {
+          throw new Error("FS operation failed");
+        }
+      });
+    }
+  });
 };
 
 await copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,17 @@
+import fs from "fs";
+
 const create = async () => {
-    // Write your code here 
+  const path = import.meta.dirname;
+
+  fs.stat(`${path}/files/fresh.txt`, function (err) {
+    if (err) {
+      fs.writeFile(`${path}/files/fresh.txt`, "I am fresh and young", (err) => {
+        if (err) return err;
+      });
+    } else {
+      throw new Error("FS operation failed");
+    }
+  });
 };
 
 await create();
