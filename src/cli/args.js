10b98f1b72e163e76ad2a42155239e0ff47de0d1
DmitriiTMS <dmitrii12_88@mail.ru>
2024-09-24 07:09:33 +0300

    feat: add cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,23 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+
+  console.log(args);
+
+  const parsedArgs = {};
+
+  for (let i = 0; i < args.length; i++) {
+    if (args[i].startsWith("--")) {
+      const propName = args[i].slice(2);
+      const value = args[i + 1];
+      parsedArgs[propName] = value;
+      i++;
+    }
+  }
+  const output = Object.entries(parsedArgs)
+    .map(([key, value]) => `${key} is ${value}`)
+    .join(", ");
+
+  console.log(output);
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,16 @@
 const parseEnv = () => {
-    // Write your code here 
+  const envVariables = process.env;
+
+  const rssVariables = Object.entries(envVariables)
+    .filter(([key]) => key.startsWith("RSS_"))
+    .map(([key, value]) => `${key}=${value}`)
+    .join("; ");
+
+  if (rssVariables) {
+    console.log(rssVariables);
+  } else {
+    console.log("No RSS_ environment variables found.");
+  }
 };
 
 parseEnv();
