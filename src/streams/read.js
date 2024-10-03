776c7106894b39ac78300951f37a8ae8aa7a32ef
DmitriiTMS <dmitrii12_88@mail.ru>
2024-10-03 23:11:29 +0300

    add read streame file

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,23 @@
+import fs from "fs";
+
 const read = async () => {
-    // Write your code here 
+    
+  const readStream = fs.createReadStream(
+    `${import.meta.dirname}\\files\\fileToRead.txt`,
+    { encoding: "utf8" }
+  );
+
+  readStream.on("data", (chunk) => {
+    process.stdout.write(chunk.toString());
+  });
+
+  readStream.on("error", (err) => {
+    console.error("Ошибка чтения файла:", err);
+  });
+
+  readStream.on("end", () => {
+    console.log('\n');
+  });
 };
 
 await read();
